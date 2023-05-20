49ddca490c8b18575ecc25f10bc90e4700535590
chetan.sharma <chetan.sharma@tavant.com>
2023-05-20 10:25:21 +0530

    Basic version of b search

---
+++ bsearch/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::vec;
+
+fn main() {
+    let vec = vec![1, 2, 3, 4, 7, 9, 13];
+    let mut search = String::new();
+
+    io::stdin().read_line(&mut search).expect("Failed to read line");
+
+    let search = match search.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+
+    let result = binary_search(&vec, search);
+    if result < vec.len() && vec[result] == search {
+        println!("Found {} at index {}", search, result);
+    } else {
+        println!("{} was not found in the vector", search);
+    }
+}
+
+fn binary_search(search_vector: &Vec<i32>, find: i32) -> usize {
+    let mut lower_bound  = 0;
+    let mut upper_bound = search_vector.len() - 1;
+
+    while lower_bound < upper_bound {
+        let mid: usize = (lower_bound + upper_bound) / 2;
+        if search_vector[mid] == find {
+           return mid;
+        } else if search_vector[mid] < find {
+            lower_bound = mid + 1;
+        } else {
+            upper_bound = mid - 1;
+        }
+    }
+    lower_bound
+}
