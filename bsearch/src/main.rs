2205adb75f998f1b9b5dd471e03c8a56e70f6942
chetan.sharma <chetan2309@gmail.com>
2023-05-25 14:20:48 +0530

    Added logic to read search vectore from a file

--- bsearch/src/main.rs
+++ bsearch/src/main.rs
@@ -1,9 +1,21 @@
+use std::fs::File;
 use std::io;
-use std::vec;
+use std::io::Read;
 use std::cmp::Ordering;
 
 fn main() {
-    let vec = vec![1, 2, 3, 4, 7, 9, 13];
+    // let vec = vec![1, 2, 3, 4, 7, 9, 13];
+    let binary_data = match read_data_from_file() {
+        Ok(contents) => contents,
+        Err(_) => {
+            print!("Error reading....");
+            return;
+        }
+    };
+    let vec = binary_data
+      .split(',')
+      .map(|s| s.trim().parse().expect("Error....."))
+      .collect();
     let mut search = String::new();
 
     io::stdin().read_line(&mut search).expect("Failed to read line");
@@ -14,24 +26,41 @@ fn main() {
     };
 
     let result = binary_search(&vec, search);
-    if result < vec.len() && vec[result] == search {
-        println!("Found {} at index {}", search, result);
-    } else {
+    println!("Result: {}", result);
+    if result == -1 {
         println!("{} was not found in the search vector", search);
+    } else {
+        println!("Found {} at index {}", search, result);
     }
 }
 
-fn binary_search(search_vector: &Vec<i32>, find: i32) -> usize {
+fn read_data_from_file() -> Result<String, io::Error> {
+    let binary_data_file = File::open("binary_search_data.txt");
+    let mut binary_data: File = match binary_data_file {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(err)
+        },
+    };
+
+    let mut data = String::new();
+    match binary_data.read_to_string(&mut data) {
+        Ok(_) => Ok(data),
+        Err(e) => Err(e)
+    }
+}
+
+fn binary_search(search_vector: &Vec<i32>, find: i32) -> isize {
     let mut lower_bound  = 0;
     let mut upper_bound = search_vector.len() - 1;
 
-    while lower_bound < upper_bound {
-        let mid: usize = (lower_bound + upper_bound) / 2;
+    while lower_bound <= upper_bound {
+        let mid = (lower_bound + upper_bound) / 2;
         match mid.cmp(&(find as usize)) {
             Ordering::Less => lower_bound = mid + 1,
             Ordering::Greater => upper_bound = mid - 1,
-            Ordering::Equal => return mid,
+            Ordering::Equal => return mid as isize,
         }
     }
-    lower_bound
+    -1
 }
